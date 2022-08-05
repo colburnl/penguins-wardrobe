68fb6c4cde4a753c9fe7d608c999cbe7238d3e0f
darkkroast <83790984+colburnl@users.noreply.github.com>
2022-08-05 19:39:35 -0400

    Add files via upload

---
+++ config_ppas.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Adds this following PPA's. Also More on the list if want to add later on.
+
+sudo add-apt-repository ppa:canonical-kernel-team/ppa -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo add-apt-repository ppa:team-xbmc/ppa -y
+sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora  -y
+sudo add-apt-repository ppa:kubuntu-ppa/backports -y
+sudo add-apt-repository ppa:kubuntu-ppa/ppa -y
+sudo add-apt-repository ppa:linuxuprising/ppa -y
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
