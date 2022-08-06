b7f382a85bf3328017cddced34bf5a0251654d42
darkkroast <83790984+colburnl@users.noreply.github.com>
2022-08-05 20:10:57 -0400

    Add files via upload

---
+++ costumes/junco/README.md
@@ -0,0 +1,9 @@
+# colibri
+
+Created for my development needs, particularly for eggs.
+
+It contains many firmwares and can be used on real computers.
+
+# background
+![junco](./dirs/usr/share/backgrounds/junco/drkuntu-2.png)
+
---
+++ costumes/junco/manjaro-junco.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# temporary script to get colibri in manjaro
+#
+
+clear
+COSTUME="junco"
+MY_USERNAME=$(logname)
+echo "MY_USERNAME: ${MY_USERNAME}"
+
+MY_USERHOME="/home/${MY_USERNAME}"
+echo "MY_USERHOME: ${MY_USERHOME}"
+
+pacman -Syu \
+libxfce4ui-gtk3 \
+lightdm \
+lightdm-gtk-greeter \
+qt5ct \
+spice-vdagent \
+shellcheck \
+thunar \
+xarchiver \
+xfce4-appfinder \
+xfce4-notifyd \
+xfce4-panel \
+xfce4-pulseaudio-plugin \
+xfce4-session \
+xfce4-settings \
+xfce4-terminal \
+xfce4-whiskermenu-plugin \
+xfconf \
+xfdesktop-gtk3 \
+xfwm4 \
+xdg-user-dirs \
+xorg-server 
+
+pacman -Syu  spice-vdagent network-manager-applet
+
+# eggs-dev
+pacman -Syu vscode nodejs npm 
+npm install pnpm -g
+
+systemctl enable NetworkManager
+systemctl enable lightdm
+
+
+# copy configuration from dirs
+cp ./dirs/* / -R
+
+# copy configuration from dirs to MY_USERHOME
+# `rsync -avx  ${this.costume}/dirs/etc/skel/.config /home/${user}/`
+rsync -avx ./dirs/etc/skel/.config "${MY_USERHOME}"/
+
+# config ligghtdn $COSTUME $MY_USERHOME
+../../scripts/config_lightdm.sh "${COSTUME}" "${MY_USERHOME}"
+
+# config desktop links $MY_USERHOME
+../../scripts/config_desktop_link.sh "${MY_USERHOME}"
+
+# Reimpostazione diritti
+chown "${MY_USERNAME}:${MY_USERNAME}" "${MY_USERHOME}" -R
